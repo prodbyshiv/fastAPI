58e046c9025f9523597a0faddb15749da9d65064
shivam <shivamtiwarisgbt@gmail.com>
2026-06-10 07:48:45 +0530

    Connect FastAPI routes with SQLite database

--- student_managment_api/main.py
+++ student_managment_api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field, EmailStr
+from database import SessionLocal
+from models import StudentDB
 
 class Student(BaseModel):
     name: str = Field(min_length=3)
@@ -16,24 +18,35 @@ class StudentResponse(BaseModel):
 
 app = FastAPI()
 
-students = []
+
 
 @app.post("/students")
+#FastAPI, create a Student object from the incoming JSON and give it to me.
 def add_student(student: Student):
+# function created that will store student obj of type Student
+    db  = SessionLocal() # created a session to interact with database
+    #to add something to database u can't send a pydantic object so you craeted db_sudent
+    # # create a StudentDB object using data from the Pydantic student object
+    db_student = StudentDB(
 
-    # students.append(student)
-    student_dict = student.model_dump() # convert student in to a dictionary
-    student_dict["id"] = len(students) + 1 # now student.id = length of the student means? +1.
-    students.append(student_dict) # now append that student dic
+        name=student.name,
+        age=student.age,
+        email=student.email,
+        cgpa=student.cgpa
+    )
+    db.add(db_student) # it expects an SQLalchemy model not pydantic model.
+    db.commit()
     return {
 
         "message": "student added successfully"
     }
 
-@app.get("/students", response_model=list[StudentResponse]) #I expect a LIST. Every item inside that list
-# should match StudentResponse.
+@app.get("/students", response_model=list[StudentResponse]) 
+#Whatever I return should be sent back as a list of StudentResponse objects.
+def get_students():
 
+    db = SessionLocal()
 
-def get_student():
+    students = db.query(StudentDB).all() # give evrything from database in StudentDB format
 
     return students
