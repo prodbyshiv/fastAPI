9f332390f86f5cd1926c9892f26d81fe81be9564
shivam <shivamtiwarisgbt@gmail.com>
2026-06-11 06:09:51 +0530

    Implement complete CRUD operations with SQLite

--- student_managment_api/main.py
+++ student_managment_api/main.py
@@ -50,3 +50,57 @@ def get_students():
     students = db.query(StudentDB).all() # give evrything from database in StudentDB format
 
     return students
+
+@app.get("/students/{student_id}")
+def get_students_by_id(student_id: int):
+    db  = SessionLocal()
+    student = (
+        db.query(StudentDB)
+        .filter(StudentDB.id == student_id)
+        .first()
+    )
+    return student
+
+@app.delete("/students/{student_id}")
+def delete_students(student_id: int):
+    db  = SessionLocal()
+    student = (
+        db.query(StudentDB)
+        .filter(StudentDB.id == student_id)
+        .first()
+    )
+    # this first() always return a StudentDb object
+    db.delete(student)
+
+    db.commit()
+
+
+    if student is None:
+        return {"message": "Student not found"}
+
+    return {
+        "message": "student deleted"
+    }
+
+@app.put("/students/{student_id}")
+def update_student(student_id: int, updated_student: Student):
+
+    db = SessionLocal()
+
+    student = (
+        db.query(StudentDB)
+        .filter(StudentDB.id == student_id)
+        .first()
+    )
+
+    if student is None:
+        return {"message": "Student not found"}
+
+    student.name = updated_student.name
+    student.age = updated_student.age
+    student.email = updated_student.email
+    student.cgpa = updated_student.cgpa
+
+    db.commit()
+
+    return {"message": "Student updated"}
