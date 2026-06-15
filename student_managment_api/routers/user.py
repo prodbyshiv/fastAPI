8e8e046bfd5436e9cf60619e205a581ab1042b82
shivam <shivamtiwarisgbt@gmail.com>
2026-06-15 06:33:20 +0530

    Implement password hashing and verification in user registration and login

--- student_managment_api/routers/user.py
+++ student_managment_api/routers/user.py
@@ -3,6 +3,8 @@ from student_managment_api.database import SessionLocal
 from student_managment_api.models.user_db import UserDB
 from student_managment_api.schemas.user import UserCreate
 from student_managment_api.schemas.user import UserLogin
+from student_managment_api.auth import hash_password
+from student_managment_api.auth import verify_password
 
 router = APIRouter()
 
@@ -14,7 +16,7 @@ def register(user: UserCreate):
     db_user = UserDB(
         username=user.username,
         email=user.email,
-        password=user.password
+        password=hash_password(user.password)
     )
 
     db.add(db_user)
@@ -46,8 +48,14 @@ def login(user: UserLogin):
 
     if db_user is None:
         return {"message": "User not found"}
-
-    if db_user.password != user.password:
+    
+    print(user.password)
+    print(db_user.password)
+    
+    if not verify_password(
+    user.password,
+    db_user.password
+):
         return {"message": "Invalid password"}
 
     return {"message": "Login successful"}
